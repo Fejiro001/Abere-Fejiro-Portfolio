429a3bace82252e18bf8ce160839fab7f47d381a
Abere Fejiro <aberefejiro@gmail.com>
2023-05-14 23:02:45 +0100

    about me additions

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 var menu = document.getElementById("menu");
+var github = document.getElementById("github");
+var linkedin = document.getElementById("linkedin");
+var twitter = document.getElementById("twitter");
+var resume = document.getElementById("resume");
 
 function toggleFunction() {
-  menu.classList.toggle("change");
+  menu.classList.toggle("change")
+}
+
+function gitHub() {
+  gitHub.onclick = window.open("https://github.com/Fejiro001")
+}
+
+function linkedIn() {
+  linkedin.onclick = window.open("https://www.linkedin.com/in/fejiro-abere-487b08161/")
+}
+
+function twitTer() {
+  twitTer.onclick = window.open("https://twitter.com/aberefejiro")
+}
+
+function reSume() {
+  resume.onclick = window.open("https://drive.google.com/file/d/16ua7cmVPR2ziV_b3nAHqhC6yRlNkN_5c/view?usp=sharing")
 }
